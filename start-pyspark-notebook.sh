77b9febf5ef7eb127a49524526ba8d3738258a49
Syed A Bilgrami <bilgrami@gmail.com>
2023-02-21 23:03:28 -0800

    --nop fixed unix CRLF

--- start-pyspark-notebook.sh
+++ start-pyspark-notebook.sh
@@ -1,8 +1,6 @@
 echo running jupyter/pyspark-notebook:latest
 docker run --name notebook -it --rm \
     -p 8888:8888 \
-    -v work:/home/jovyan/work \
+    -v `pwd`/work:/home/jovyan/work \
+    -e GRANT_SUDO=yes \
     bilgrami/start-ml:v1
-
-# run this in another window to copy notebooks
-# docker cp ${PWD}/work/ notebook:/home/jovyan/work/
